5fbd6b8d2cc7afd18588ccc727008a1fc47d2905
Valentin Piho <val.piho@gmail.com>
2020-10-13 22:05:30 +0300

    Adding instructor to database

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,8 +1,34 @@
 package demo;
 
+import entity.Instructor;
+import entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Instructor instructor = new Instructor("Val", "Piho", "val.piho@gmail.com");
+            InstructorDetail instructorDetail = new InstructorDetail("http://www.pibox.ee/youtube", "Basketball");
+            instructor.setInstructorDetail(instructorDetail);
+
+            session.save(instructor);
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
